f81e83f2ddf653c15f85499ab589a95f6b620ac5
Matthaios Topalidis <75705991+mathaiostop23@users.noreply.github.com>
2023-11-17 19:17:12 +0200

    TrafficSignClassification.py

--- TrafficSignClassification.py
+++ TrafficSignClassification.py
@@ -17,9 +17,9 @@ np.random.seed(42)
 
 class_names = np.array(['Speed limit (20km/h)', 'Speed limit (30km/h)', 'Speed limit (50km/h)', 'Speed limit (60km/h)', 'Speed limit (70km/h)', 'Speed limit (80km/h)', 'End of speed limit (80km/h)', 'Speed limit (100km/h)', 'Speed limit (120km/h)', 'No passing', 'No passing for vehicles over 3.5 metric tons', 'Right-of-way at the next intersection', 'Priority road', 'Yield', 'Stop', 'No vehicles', 'Vehicles over 3.5 metric tons prohibited', 'No entry', 'General caution', 'Dangerous curve to the left', 'Dangerous curve to the right', 'Double curve', 'Bumpy road', 'Slippery road', 'Road narrows on the right', 'Road work', 'Traffic signals', 'Pedestrians', 'Children crossing', 'Bicycles crossing', 'Beware of ice/snow', 'Wild animals crossing', 'End of all speed and passing limits', 'Turn right ahead', 'Turn left ahead', 'Ahead only', 'Go straight or right', 'Go straight or left', 'Keep right', 'Keep left', 'Roundabout mandatory', 'End of no passing', 'End of no passing by vehicles over 3.5 metric tons'])
 
-data_dir = '/Users/mathaios/Desktop/TrafficSignModelClassification'
-train_path = '/Users/mathaios/Desktop/TrafficSignModelClassification/Train'
-test_path = '/Users/mathaios/Desktop/TrafficSignModelClassification/Test'
+data_dir = './TrafficSignModelClassification'
+train_path = './TrafficSignModelClassification/Train'
+test_path = './TrafficSignModelClassification/Test'
 
 
 input_shape = (32, 32, 3)
@@ -100,7 +100,7 @@ history = model.fit(aug.flow(X_train, y_train, batch_size=batch_size), epochs=ep
 
 
 
-test = pd.read_csv('/Users/mathaios/Desktop/TrafficSignModelClassification/Test.csv')
+test = pd.read_csv('./TrafficSignModelClassification/Test.csv')
 labels = test["ClassId"].values
 imgs = test["Path"].values
 
